68233233ea6e79a201821665c1dfa78699966baf
Gabriel Ibenye <gabrielibenye@gmail.com>
2023-02-20 01:28:22 +0100

    extrated chat textfield widget

--- lib/features/home/pages/new_chat_home_page.dart
+++ lib/features/home/pages/new_chat_home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/chat_text_field.dart';
+
 class NewChatHomePage extends StatelessWidget {
   const NewChatHomePage({Key? key}) : super(key: key);
 
@@ -7,37 +9,7 @@ class NewChatHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(children: [
       Expanded(child: Container()),
-      Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: TextField(
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                onSubmitted: (value) {},
-                decoration: const InputDecoration(
-                  contentPadding: EdgeInsets.all(18),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(12)),
-                      borderSide: BorderSide(
-                        color: Colors.teal,
-                      )),
-                  // suffixIcon: Icon(Icons.send),
-                  hintText: 'Tell us about yourself',
-                  suffixStyle: TextStyle(color: Colors.green),
-                ),
-              ),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.send),
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
-            )
-          ],
-        ),
-      ),
+      const ChatTextField(),
     ]);
   }
 }
---
+++ lib/features/home/widgets/chat_text_field.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ChatTextField extends StatelessWidget {
+  const ChatTextField({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              onSubmitted: (value) {},
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.all(18),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    borderSide: BorderSide(
+                      color: Colors.teal,
+                    )),
+                // suffixIcon: Icon(Icons.send),
+                hintText: 'Tell us about yourself',
+                suffixStyle: TextStyle(color: Colors.green),
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.send),
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+          )
+        ],
+      ),
+    );
+  }
+}
